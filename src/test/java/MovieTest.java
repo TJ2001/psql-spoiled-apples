b5c33a9b261da254305d502a3dbaf361439db688
Ryan Vinyard and tim jung <ryan@>
2016-09-21 15:09:28 -0700

    pass more tests

--- src/test/java/MovieTest.java
+++ src/test/java/MovieTest.java
@@ -40,6 +40,45 @@ public class MovieTest {
     mMovie.save();
     assertTrue(mMovie.getId() > 0);
   }
+
+//new tests
+
+@Test
+ public void find_returnsMovieWithSameId_secondMovie() {
+   Movie secondMovie = new Movie("Happy Gilmore", "comedy");
+   secondMovie.save();
+   assertEquals(Movie.find(secondMovie.getId()), secondMovie);
+ }
+
+  @Test
+  public void equals_returnsTrueIfNamesAretheSame() {
+    Movie secondMovie = new Movie("Indian Jones", "Parody");
+    assertTrue(firstMovie.equals(secondMovie));
+  }
+
+  @Test
+  public void save_savesIntoDatabase_true() {
+    myMovie.save();
+    assertTrue(Movie.all().get(0).equals(mMovie));
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    mMovie.save();
+    Movie savedMovie = Movie.all().get(0);
+    assertEquals(mMovie.getId(), savedMovie.getId());
+  }
+
+  @Test
+  public void getReviews_retrievesALlReviewsFromDatabase_reviewsList() {
+    mMovie.save();
+    Review firstReview = new Review("Reviewer123", "This movie is awesome.", mMovie.getId());
+    firstReview.save();
+    Review secondReview = new Review("Lane21", "This movie sucks.", myMovie.getId());
+    secondReview.save();
+    Review[] reviews = new Review[] { firstReview, secondReview };
+    assertTrue(myMovie.getReviews().containsAll(Arrays.asList(reviews)));
+  }
 }
 
 //We were going to change "review" to "reviews" (see console)
